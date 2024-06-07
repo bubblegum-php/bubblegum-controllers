a2455b591dce0843f0181f3d0965d2505e0fab31
Oleksii Fedorenko <lankrf@gmail.com>
2024-06-07 14:23:37 +0300

    Add controller main class

---
+++ src/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Bubblegum;
+
+use Bubblegum\Routes\RoutedComponent;
+
+class Controller extends RoutedComponent
+{
+    public function referToDestination(Request $request): string
+    {
+        $response = call_user_func_array([$this, $this->destinationName], func_get_args());
+        return match (gettype($response)) {
+            'string' => $response,
+            default => json_encode($response),
+        };
+    }
+}
