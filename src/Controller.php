aa5b826acfbd5c9b039b223b4978ae9b58236c68
Oleksii Fedorenko <lankrf@gmail.com>
2024-06-19 00:13:21 +0300

    Reflection call for controller methods

--- src/Controller.php
+++ src/Controller.php
@@ -3,12 +3,19 @@
 namespace Bubblegum;
 
 use Bubblegum\Routes\RoutedComponent;
+use ReflectionMethod;
+use InvalidArgumentException;
+use ReflectionException;
 
 class Controller extends RoutedComponent
 {
+    /**
+     * @throws ReflectionException
+     */
     public function content(Request $request, array $data = []): string
     {
-        $response = call_user_func_array([$this, $this->destinationName], func_get_args());
+        $data['request'] = $request;
+        $response = $this->callDestinationWithArguments($data);
         switch (gettype($response)) {
             case 'string':
                 return $response;
@@ -16,5 +23,27 @@ class Controller extends RoutedComponent
                 header('Content-Type: application/json');
                 return json_encode($response);
         }
+    }
+
+    /**
+     * @param array $args
+     * @return mixed
+     * @throws ReflectionException
+     */
+    protected function callDestinationWithArguments(array $args): mixed
+    {
+        $reflection = new ReflectionMethod($this, $this->destinationName);
+        $orderedArgs = [];
+        foreach ($reflection->getParameters() as $param) {
+            $name = $param->getName();
+            if (array_key_exists($name, $args)) {
+                $orderedArgs[] = $args[$name];
+            } elseif ($param->isOptional()) {
+                $orderedArgs[] = $param->getDefaultValue();
+            } else {
+                throw new InvalidArgumentException("Missing parameter \"$name\" for method \"{$this->destinationName}\" in controller " . get_class($this));
+            }
+        }
+        return $reflection->invokeArgs($this, $orderedArgs);
     }
 }
